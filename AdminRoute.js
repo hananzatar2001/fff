4c5e4300ec207b3b5e6ab8d4665908db5894bb57
hanan zatar <90716074+hananzatar2001@users.noreply.github.com>
2022-05-01 01:57:13 +0300

    Add files via upload

---
+++ AdminRoute.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { useSelector } from 'react-redux';
+import { Redirect,Route } from 'react-router-dom';
+
+export default function PrivateRoute ({component:component, ...rest}){
+  const userSignin = useSelector((state) => state.userSignin);
+  const { userInfo } = userSignin;
+  return (
+  <Route
+  {...res}
+  render={(props)=>
+    userInfo && userInfo.isAdmin ? (
+        <component{...props}></component>
+    ):(<Redirect to="/signin"/>
+    )
+  }
+    ></Route>
+  );
+}
